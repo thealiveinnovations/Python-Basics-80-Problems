3118f78c131f983afa899f016335c97d51feb6cd
The Alive Innovations <managethealive@gmail.com>
2024-11-01 18:28:10 +0600

    Create itearate_dictionary.py

---
+++ Basic_Python_Solutions/Dictionaries_&_Sets/itearate_dictionary.py
@@ -0,0 +1,7 @@
+#Write a program to iterate through all key-value pairs in a dictionary.
+my_info = {'first_name':'adity','last_name':'abdullah','age':22,'height':'1.52m'}
+
+for key,value in my_info.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+    
