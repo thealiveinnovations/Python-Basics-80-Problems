439d0be6810241cbb14a87c1754e3f778af3e4c5
The Alive Innovations <managethealive@gmail.com>
2024-11-01 15:08:02 +0600

    Create even_or_odd.py

---
+++ Basic_Python_Solutions/Control_Flow/even_or_odd.py
@@ -0,0 +1,7 @@
+#Write a program to check if a number is even or odd.
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+   print(f"\nThe number {num} is even.")
+else:
+   print(f"\nThe number {num} is odd.")
