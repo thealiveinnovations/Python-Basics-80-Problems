69315b76593fc47f58274a39b651dc6f0c92df98
The Alive Innovations <managethealive@gmail.com>
2024-11-01 11:23:00 +0600

    Create multiply_two_numbers.py

---
+++ Basic_Python_Solutions/Arithmetic_Operations/multiply_two_numbers.py
@@ -0,0 +1,7 @@
+#Write a program to multiply two numbers.
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+mul = num_1 * num_2
+
+print(f"The multiplication of {num_1} and {num_2} is {mul}.")
