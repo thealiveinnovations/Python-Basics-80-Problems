7d68527ed511bd5ddae58610248e386153c1fb9a
The Alive Innovations <managethealive@gmail.com>
2024-11-01 14:50:25 +0600

    Create remainder.py

---
+++ Basic_Python_Solutions/Arithmetic_Operations/remainder.py
@@ -0,0 +1,7 @@
+#Write a program to find the remainder when one number is divided by another.
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+remainder = num_1 % num_2
+
+print(f"The remainder of {num_1} and {num_2} is {remainder}.")
