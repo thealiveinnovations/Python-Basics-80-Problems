88ef00905232c9d324f0bedfc3680ffc4e94294e
The Alive Innovations <managethealive@gmail.com>
2024-11-01 22:03:11 +0600

    Create create_tuple.py

---
+++ Basic_Python_Solutions/Lists_&_Tuples/create_tuple.py
@@ -0,0 +1,7 @@
+#Write a program to create a tuple with different data types and print each element.
+personal_info = ('John','Smith',22,1.72)
+print("Personal informations: ")
+print(f"\tFirst name: {personal_info[0]}")
+print(f"\tLast name: {personal_info[1]}")
+print(f"\tAge: {personal_info[2]}")
+print(f"\tHeight: {personal_info[3]}m")
