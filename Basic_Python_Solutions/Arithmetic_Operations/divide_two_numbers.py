7abd21133d7205de5b9dcc342f2df0e448f2a41d
The Alive Innovations <managethealive@gmail.com>
2024-11-01 14:49:04 +0600

    Create divide_two_numbers.py

---
+++ Basic_Python_Solutions/Arithmetic_Operations/divide_two_numbers.py
@@ -0,0 +1,7 @@
+#Write a program to divide two numbers.
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+div = num_1 / num_2
+
+print(f"The addition of {num_1} and {num_2} is {div}.")
