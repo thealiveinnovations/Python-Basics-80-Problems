bd1a782632a7700b41b90e74f90cc2c7c407ce61
The Alive Innovations <managethealive@gmail.com>
2024-11-01 18:21:06 +0600

    Create remove_key.py

---
+++ Basic_Python_Solutions/Dictionaries_&_Sets/remove_key.py
@@ -0,0 +1,5 @@
+#Write a program to remove a key from a dictionary.
+my_info = {'first_name':'John','last_name':'Smith','age':22,'location':'Chicago'}
+print(my_info)
+del my_info['location']
+print(my_info)
