4da439ce4b8c6cbb10d689d6e00249d08f86da5b
The Alive Innovations <managethealive@gmail.com>
2024-11-01 18:15:43 +0600

    Create access_key_value.py

---
+++ Basic_Python_Solutions/Dictionaries_&_Sets/access_key_value.py
@@ -0,0 +1,6 @@
+#Write a program to access a value in a dictionary by it's key.
+my_info = {'first_name':'John','last_name':'Smith','age':22}
+print("Personal informations: ")
+print(f"\tFirst name: {my_info['first_name']}")
+print(f"\tLast name: {my_info['last_name']}")
+print(f"\tAge: {my_info['age']}")
