ef63eb5cca1bcee30b2a169badbac8fa6e8bdbda
The Alive Innovations <managethealive@gmail.com>
2024-11-01 20:13:03 +0600

    Create create_set.py

---
+++ Basic_Python_Solutions/Dictionaries_&_Sets/create_set.py
@@ -0,0 +1,6 @@
+#Write a program to create a set and add elements to it.
+book_collection = {'python crash course','grokking algorithms','fluent python'}
+
+book_collection.add('introduction to algorithm')
+book_collection.add('deep learning')
+print(book_collection)
